bd924322c50067795dfbec741ccb98cab8a5a57e
yaodehaung <sef1548@gmail.com>
2020-11-27 17:18:30 +0800

    feat: init project

---
+++ READM.md
@@ -0,0 +1,11 @@
+ # Step One
+ 
+ mvn compile
+ 
+ 
+ # Step Two
+ 
+  mvn exec:java -Dexec.mainClass="com.logicbig.example.ReactorGettingStartedExample"
+
+ 
+ 
---
+++ src/main/java/com/logicbig/example/ReactorGettingStartedExample.java
@@ -0,0 +1,16 @@
+package com.logicbig.example;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public class ReactorGettingStartedExample {
+    public static void main(String[] args) {
+        System.out.println("-- Mono example --");
+        Mono<String> mono = Mono.just("test mono");
+        mono.subscribe(System.out::println);
+
+        System.out.println("-- Flux example --");
+        Flux<Integer> flux = Flux.just(1, 2, 3);
+        flux.subscribe(System.out::println);
+    }
+}
